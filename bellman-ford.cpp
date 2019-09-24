97716be7177abd6f14a71df1a120d200aee966d3
mohit <39158356+mohitacecode@users.noreply.github.com>
2019-09-24 17:23:21 +0530

    Update bellman-ford.cpp
    Bellman ford algorithms is single source shortest path in directed graph algorithms.It works for negative edge also unlike dijikstras.
    Its fail in case if negative weighted cycle is present in graph.
    
    Time complexity:O(n^2)->Best
    If graph is complete graph then the algorithm will take O(n^3) time.

--- bellman-ford.cpp
+++ bellman-ford.cpp
@@ -1,10 +1,18 @@
+/*
+PROGRAM FOR BELLMAN-FORD-ALGORITHMS
+@author MOHIT GUPTA
+Date:-24th september 2019
+*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
 #include <bits/stdc++.h> 
+//FUNCTION TO ADD AN EDGE TO GRAPH
 void addedge(vector<pair<int,int>> adj[],int u,int v,int w){
     adj[u].push_back(make_pair(v,w));
 }
+//FUNCTION FOR RELAXATION OF AN EDGE
 void relax(int d[],int u,int v,int w){
     if(d[v] > d[u]+w){
         d[v] = d[u]+w;
@@ -12,8 +20,10 @@ void relax(int d[],int u,int v,int w){
 }
 int main(){
     int n,i,j,u,v,w,choice;
+    //TAKING INPUT FOR NUMBER OF VERTICES n=number of vertices
     cout<<"Please Enter the no. of vertices"<<endl;
     cin >> n;
+   
     int d[n];
     vector<pair<int,int>> adj[n];
     while(1){
